af9e07bb17c277b06b715ac9edd5d2e07c38e04c
Andre Bauzil <andregbauzil@gmail.com>
2023-12-27 12:00:58 -0300

    Adjust on the domain variable

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://658ad556ba789a962237f3ae.mockapi.io",
-    DOMAIN_ORIGIN: "https://localhost:3000",
+    DOMAIN_ORIGIN: "http://localhost:3000",
   },
 };
 
