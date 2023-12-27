000069bc422c2bcf1b4f38852b859601236a5ce7
Andre Bauzil <andregbauzil@gmail.com>
2023-12-27 12:02:58 -0300

    Add styling and structure for main page

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,6 @@
+import styles from './heroesList.module.scss'
+
+import { spidermanFont } from "@/fonts"
 import { IHeroData } from "@/interfaces/heroes"
 
 interface IProps {
@@ -7,7 +10,7 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1>PERSONAGENS</h1>
+      <h1 className={`${spidermanFont.className} ${styles.title}`}>PERSONAGENS</h1>
       {heroes.map(hero => (
         <p key={hero.id}>{hero.name}</p>
       ))}
