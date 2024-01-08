499fdb37706032c201707e4d361d134c8d68da3f
AndreBauzil <Andregbauzil@gmail.com>
2024-01-08 16:39:54 -0300

    feat: add animation on heroes' carousel, also func to get style based on hero's position

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import HeroDetails from "../HeroDetails";
 import HeroPicture from "../HeroPicture";
@@ -8,6 +9,13 @@ import HeroPicture from "../HeroPicture";
 import styles from "./Carousel.module.scss"
 
 import { IHeroData } from "@/interfaces/heroes";
+
+
+enum enPosition {
+  FRONT = 0,
+  MIDDLE = 1,
+  BACK = 2
+}
 interface IProps {
   heroes: IHeroData[];
   activeId: string;
@@ -42,11 +50,25 @@ export default function Carousel({ heroes, activeId }: IProps) {
       <div className={styles.container}>
         <div className={styles.carousel}>
           <div className={styles.wrapper} onClick={() => handleChangeActiveIndex(1)}>
-            {visibileItems.map((item) => (
-              <div key={item.id} className={styles.hero}>
-                <HeroPicture hero={item} />
-              </div>
-            ))}
+            <AnimatePresence mode="popLayout">
+              {visibileItems.map((item, position) => (
+                <motion.div
+                  key={item.id}
+                  className={styles.hero}
+                  initial={{ x: -1500, scale: 0.75 }}
+                  animate={{ x: 0, ...getItemStyles(position) }}
+                  exit={{
+                    x: -300,
+                    opacity: 0,
+                    scale: 1,
+                    left: "-20%"
+                  }}
+                  transition={{ duration: 0.8 }}
+                >
+                  <HeroPicture hero={item} />
+                </motion.div>
+              ))}
+            </AnimatePresence>
           </div>
         </div>
 
@@ -58,3 +80,26 @@ export default function Carousel({ heroes, activeId }: IProps) {
   )
 }
 
+const getItemStyles = (position: enPosition) => {
+  if (position === enPosition.FRONT) return {
+    zIndex: 3,
+    filter: "blur(10px)",
+    scale: 1.2
+  };
+
+  if (position === enPosition.MIDDLE) return {
+    zIndex: 2,
+    left: 350,
+    scale: 0.8,
+    top: "-10%"
+  };
+
+  return {
+    zIndex: 1,
+    filter: "blur(10px)",
+    left: 235,
+    top: "-20%",
+    scale: 0.6,
+    opacity: 0.8
+  };
+}
