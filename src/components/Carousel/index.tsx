5c76e1688990b8d3150696e498fbd59792cbfbd2
Andre Bauzil <andregbauzil@gmail.com>
2024-01-09 11:36:56 -0300

    feat: change background image, details and play audio when the hero changes

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 import HeroDetails from "../HeroDetails";
@@ -24,9 +24,37 @@ interface IProps {
 export default function Carousel({ heroes, activeId }: IProps) {
   const [visibileItems, setVisibileItems] = useState<IHeroData[] | null>(null)
   const [activeIndex, setActiveIndex] = useState<number>(
-    heroes.findIndex((hero) => hero.id === activeId)
+    heroes.findIndex((hero) => hero.id === activeId) - 1
   )
 
+  const transitionAudio = useMemo(() => new Audio("/songs/transition.mp3"), [])
+  const voicesAudio: Record<string, HTMLAudioElement> = useMemo(
+    () => ({
+      "spider-man-616": new Audio("/songs/spider-man-616.mp3"),
+      "spider-woman-65": new Audio("/songs/spider-woman-65.mp3"),
+      "spider-man-1610": new Audio("/songs/spider-man-1610.mp3"),
+      "sp-dr-14512": new Audio("/songs/sp-dr-14512.mp3"),
+      "spider-ham-8311": new Audio("/songs/spider-ham-8311.mp3"),
+      "spider-man-90214": new Audio("/songs/spider-man-90214.mp3"),
+      "spider-man-928": new Audio("/songs/spider-man-928.mp3")
+    }),
+    []
+  )
+
+  // for heroes' audios and transisiton
+  useEffect(() => {
+    if (!visibileItems) return
+
+    transitionAudio.play();
+
+    const voiceAudio = voicesAudio[visibileItems[enPosition.MIDDLE].id]
+    if (!voiceAudio) return
+
+    voiceAudio.volume = 0.1;
+    voiceAudio.play()
+  }, [visibileItems, transitionAudio, voicesAudio])
+
+  // for visibile heroes on screen
   useEffect(() => {
     // Function to loop over hero list, also dont pass the array max length
     const indexOnArrayScope = ((activeIndex % heroes.length) + heroes.length) % heroes.length;
@@ -34,6 +62,22 @@ export default function Carousel({ heroes, activeId }: IProps) {
     const visibileItems = [...heroes, ...heroes].slice(indexOnArrayScope, indexOnArrayScope + 3)
     setVisibileItems(visibileItems)
   }, [heroes, activeIndex])
+
+  // for heroes' background image
+  useEffect(() => {
+    const htmlEl = document.querySelector("html")
+
+    if (!htmlEl || !visibileItems) return;
+
+    const currentHeroId = visibileItems[enPosition.MIDDLE].id;
+    htmlEl.style.backgroundImage = `url("/spiders/${currentHeroId}-background.png")`;
+
+    htmlEl.classList.add("hero-page");
+
+    return () => {
+      htmlEl.classList.remove("hero-page");
+    }
+  }, [visibileItems])
 
   // Changes active hero on carousel
   // newDirection = +1, rotates clockwise
@@ -72,9 +116,13 @@ export default function Carousel({ heroes, activeId }: IProps) {
           </div>
         </div>
 
-        <div className={styles.details}>
-          <HeroDetails data={heroes[0]} />
-        </div>
+        <motion.div className={styles.details}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1, duration: 2 }}
+        >
+          <HeroDetails data={visibileItems[enPosition.MIDDLE]} />
+        </motion.div>
       </div>
     </>
   )
