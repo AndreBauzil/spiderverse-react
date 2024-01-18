d1b5ba442f5f3cb29e69fd6c36c5ea5d300b39d0
Andre Bauzil <andregbauzil@gmail.com>
2024-01-18 11:47:54 -0300

    fix: to build and deploy it

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+import { GetStaticPathsResult } from "next";
 
 import Carousel from "@/components/Carousel";
 import { IHeroData } from "@/interfaces/heroes";
@@ -9,18 +9,37 @@ interface IProps {
   }
 }
 
-async function getHeroesData(): Promise<{ data: IHeroData[] }> {
+async function getHeroesData(): Promise<IHeroData[]> {
   const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`)
 
   if (!res.ok) {
     throw new Error("Failed to request heroes list")
   }
 
+  console.log(res.json());
+
+
   return res.json()
 }
 
-export default async function Hero({ params: { id } }: IProps) {
+export default async function Hero({ params: { id }, heroesData }: { params: { id: string }, heroesData: IHeroData[] }) {
   const heroes = await getHeroesData();
 
-  return <Carousel heroes={heroes.data} activeId={id} />
+  return <Carousel heroes={heroesData} activeId={id} />
+}
+
+export async function generateStaticParams(): Promise<GetStaticPathsResult> {
+  const heroes = await getHeroesData()
+
+  const paths = heroes.map(hero => ({
+    params: { id: hero.id.toString() },
+  }))
+
+  console.log(paths);
+
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
