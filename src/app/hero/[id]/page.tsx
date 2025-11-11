07e31c21998cb37db19058c5935485de2555d020
AndreBauzil <Andregbauzil@gmail.com>
2025-11-11 08:44:56 -0300

    fix(frontend): use relative paths for api calls

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 async function getHeroesData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`)
+  const res = await fetch('/api/heroes');
 
   if (!res.ok) {
     throw new Error("Failed to request heroes list")
